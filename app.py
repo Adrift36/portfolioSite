d25dbf8ca801cad3eaf1d13ceca2b20339492c8d
George Hill <blocker1.gh@gmail.com>
2021-08-11 22:59:11 +0100

    Added contact page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from os import name
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
@@ -55,9 +57,52 @@ def edit(id):
         return render_template('edit.html', project=project)
 
 
-@app.route('/home/<string:name>')
-def hello(name):
-    return f'Hello {name}'
+@app.route('/contact')
+def contact():
+    if request.method == 'POST':
+        ##contact.title = request.form['title']
+        ##contact.date = request.form['date']
+        ##project.content = request.form['content']
+        ##db.session.commit()
+        return redirect('/contact')
+    else:
+        return render_template('contact.html')
+
+
+@app.route('/c4/<string:gameSequence>')
+def bestmove(gameSequence): #temporary solution while I figure out minimax algorithms
+
+    #headers to make the request look genuine
+	headers = {
+		'authority': 'connect4.gamesolver.org',
+		'sec-ch-ua': '^\\^',
+		'accept': 'application/json, text/javascript, */*; q=0.01',
+		'x-requested-with': 'XMLHttpRequest',
+		'sec-ch-ua-mobile': '?0',
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36',
+		'sec-fetch-site': 'same-origin',
+		'sec-fetch-mode': 'cors',
+		'sec-fetch-dest': 'empty',
+		'referer': f'https://connect4.gamesolver.org/?pos={gameSequence[:-1]}',
+		'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+	#	'cookie': '',
+	}
+
+    #game state parameter
+	params = (
+		('pos', gameSequence),
+	)
+
+    #make the request
+	response = requests.get('https://connect4.gamesolver.org/solve', headers=headers, params=params)
+
+    #intepret the response to decide the best move for the robot
+	ratings = [-100 if x==100 else x for x in response.json()['score']]
+	max_value = max(ratings)
+	max_index = ratings.index(max_value)
+
+	return jsonify({'index': max_index})
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
